f9bb11715f4d5b47ff5c07633ed37b6cb89dd89f
Douglas Blackwood <douglas.blackwood@protonmail.com>
2020-11-28 22:54:17 +0100

    Création de la fonction trigger trace_changes et de la fonction qui installe les colonnes et le trigger

--- sync--0.1.sql
+++ sync--0.1.sql
@@ -20,7 +20,7 @@ $$
 	SELECT db_id FROM sync.db_id;
 $$;
 
-COMMENT ON FUNCTION sync.db_id() IS 'Retruns database ID';
+COMMENT ON FUNCTION sync.db_id() IS 'Returns database ID';
 
 CREATE OR REPLACE FUNCTION sync.is_main()
 	RETURNS boolean
@@ -31,7 +31,7 @@ $$
 	SELECT main FROM sync.db_id;
 $$;
 
-COMMENT ON FUNCTION sync.is_main() IS 'Retruns whether this database is the main database';
+COMMENT ON FUNCTION sync.is_main() IS 'Returns whether this database is the main database';
 
 CREATE OR REPLACE FUNCTION sync.is_server()
 	RETURNS boolean
@@ -42,7 +42,7 @@ $$
 	SELECT main FROM sync.db_id;
 $$;
 
-COMMENT ON FUNCTION sync.is_server() IS 'Retruns whether this database is the main database';
+COMMENT ON FUNCTION sync.is_server() IS 'Returns whether this database is the main database';
 
 CREATE OR REPLACE FUNCTION sync.is_replica()
 	RETURNS boolean
@@ -53,4 +53,57 @@ $$
 	SELECT NOT main FROM sync.db_id;
 $$;
 
-COMMENT ON FUNCTION sync.is_replica() IS 'Retruns whether this database is a replica';
+COMMENT ON FUNCTION sync.is_replica() IS 'Returns whether this database is a replica';
+
+CREATE OR REPLACE FUNCTION sync.install_tracer(_table regclass)
+	RETURNS void
+	LANGUAGE plpgsql
+AS
+$BODY$
+BEGIN
+	BEGIN
+		EXECUTE FORMAT('ALTER TABLE %I ADD COLUMN pgs_is_active BOOLEAN DEFAULT TRUE;', _table);
+	EXCEPTION WHEN duplicate_column THEN
+		RAISE NOTICE 'pgs_is_active already exists';
+	END;
+
+	BEGIN
+		EXECUTE FORMAT('ALTER TABLE %I ADD COLUMN pgs_changed_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT statement_timestamp();', _table);
+	EXCEPTION WHEN duplicate_column THEN
+		RAISE NOTICE 'pgs_changed_at already exists';
+	END;
+
+	BEGIN
+		EXECUTE FORMAT('ALTER TABLE %I ADD COLUMN pgs_synced_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT statement_timestamp();', _table);
+	EXCEPTION WHEN duplicate_column THEN
+		RAISE NOTICE 'pgs_synced_at already exists';
+	END;
+
+	EXECUTE FORMAT('DROP TRIGGER IF EXISTS pgs_trace_changes ON %I;', _table);
+	EXECUTE FORMAT(
+		$$
+			CREATE TRIGGER pgs_trace_changes
+			BEFORE INSERT OR UPDATE OR DELETE
+			ON %I
+			FOR EACH ROW
+			EXECUTE PROCEDURE sync.trace_changes()
+		$$,
+		_table);
+END;
+$BODY$;
+
+CREATE OR REPLACE FUNCTION sync.trace_changes()
+	RETURNS trigger
+	LANGUAGE plpgsql
+AS
+$BODY$
+BEGIN
+	IF TG_OP = 'DELETE' THEN
+		RETURN NULL;
+	ELSE
+		NEW.pgs_changed_at = statement_timestamp();
+		NEW.pgs_synced_at = statement_timestamp();
+		RETURN NEW;
+	END IF;
+END;
+$BODY$;
---
+++ tests/test_trigger.sql
@@ -0,0 +1,46 @@
+BEGIN;
+SELECT plan(14);
+
+CREATE TEMP TABLE people
+(
+	id INT GENERATED ALWAYS AS IDENTITY,
+	first_name TEXT,
+	last_name TEXT
+);
+
+-- Installe les colonnes et trigger
+SELECT sync.install_tracer('people');
+
+SELECT has_column('people', 'pgs_is_active', 'column pgs_is_active is missing');
+SELECT has_column('people', 'pgs_changed_at', 'column pgs_changed_at is missing');
+SELECT col_not_null('people', 'pgs_changed_at', 'column pgs_changed_at must have NOT NULL constraint');
+SELECT has_column('people', 'pgs_synced_at', 'column pgs_synced_at is missing');
+SELECT col_not_null('people', 'pgs_synced_at', 'column pgs_synced_at must have NOT NULL constraint');
+SELECT has_trigger('people', 'pgs_trace_changes', 'trigger pgs_trace_changes is missing');
+
+-- Vérifie que la fonction peut être appelée deux fois
+SELECT sync.install_tracer('people');
+
+INSERT INTO people (first_name, last_name)
+VALUES
+	('Mike', 'Tyson'),
+	('Michel', 'Zecler'),
+	('Diego', 'Maradona'),
+	('Hassan', 'Rohani'),
+	('Alex', 'Thomson');
+
+SELECT ok(pgs_is_active, 'pgs_is_active must be TRUE') FROM people LIMIT 1;
+SELECT ok(pgs_changed_at >= now(), 'wrong pgs_changed_at') FROM people LIMIT 1;
+SELECT ok(pgs_changed_at <= statement_timestamp(), 'wrong pgs_changed_at') FROM people LIMIT 1;
+SELECT ok(pgs_synced_at >= now(), 'wrong pgs_synced_at') FROM people LIMIT 1;
+SELECT ok(pgs_synced_at <= statement_timestamp(), 'wrong pgs_synced_at') FROM people LIMIT 1;
+
+DELETE FROM people;
+SELECT ok(COUNT(*) = 5) FROM people;
+
+UPDATE people SET pgs_changed_at = 'tomorrow', pgs_synced_at = 'tomorrow';
+SELECT ok(pgs_changed_at <= statement_timestamp(), 'wrong pgs_changed_at') FROM people LIMIT 1;
+SELECT ok(pgs_synced_at <= statement_timestamp(), 'wrong pgs_synced_at') FROM people LIMIT 1;
+
+SELECT * FROM finish();
+ROLLBACK;
